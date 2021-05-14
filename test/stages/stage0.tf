1c785996a66c76c9f837f233af56c9e6c0f5afdd
Aparna Mane <aparnamane@Aparnas-MBP.attlocal.net>
2021-05-14 14:06:27 -0400

    Updated path

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,7 +1,7 @@
 # Initialize the HPCS instance
 module "hpcs_init" {
   initialize = var.initialize
-  source     = "git@github.com:slzone/terraform-ibm-hpcs-initialization.git?ref=hpcs-init"
+  source     = "git@github.com:slzone/terraform-ibm-hpcs-initialization.git?ref=hpcs-init-dec"
   # source              = "https://github.com/slzone/terraform-ibm-hpcs-initialization"
   hpcs_instance_guid  = var.hpcs_instance_guid
   tke_files_path      = var.tke_files_path
@@ -16,8 +16,8 @@ module "hpcs_init" {
 
 # Upload signed keys / tke files to provided COS bucket
 module "upload_to_cos" {
-  source = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/upload-to-cos?ref=hpcs-init"
-  # source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/upload-to-cos"
+  source = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/upload-to-cos?ref=hpcs-init-dev"
+  #   source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/upload-to-cos"
   depends_on         = [module.hpcs_init]
   api_key            = var.api_key
   cos_crn            = var.cos_crn
@@ -29,8 +29,8 @@ module "upload_to_cos" {
 
 # Remove tke files from local path
 module "remove_tke_files" {
-  # source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/remove-tkefiles?ref=hpcs-init"
-  source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/remove-tkefiles"
+  source = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/remove-tkefiles?ref=hpcs-init-dev"
+  #   source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/remove-tkefiles"
   depends_on         = [module.upload_to_cos]
   tke_files_path     = var.tke_files_path
   input_file_name    = var.input_file_name
@@ -40,7 +40,7 @@ module "remove_tke_files" {
 # Apply HPCS Network and dual auth delete policies
 module "hpcs_policies" {
   #   source               = "git::https://github.com/slzone/terraform-ibm-hpcs-initialisation.git//modules/hpcs-policies"
-  source               = "git::https://github.com/slzone/terraform-ibm-hpcs-initialisation.git//modules/hpcs-policies?ref=hpcs-init"
+  source               = "git::https://github.com/slzone/terraform-ibm-hpcs-initialisation.git//modules/hpcs-policies?ref=hpcs-init-dev"
   depends_on           = [module.hpcs_init]
   region               = var.region
   resource_group_name  = var.resource_group_name
