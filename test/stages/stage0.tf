80d02c9302c60ca842d7e197290fab45de62f7fd
Aparna Mane <aparnamane@Aparnas-MBP.attlocal.net>
2021-05-14 13:13:28 -0400

    Updated module pathe in source

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,7 +1,7 @@
 # Initialize the HPCS instance
 module "hpcs_init" {
   initialize = var.initialize
-  source     = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/ibm-hpcs-initialisation?ref=hpcs-init"
+  source     = "git@github.com:slzone/terraform-ibm-hpcs-initialization.git?ref=hpcs-init"
   # source              = "https://github.com/slzone/terraform-ibm-hpcs-initialization"
   hpcs_instance_guid  = var.hpcs_instance_guid
   tke_files_path      = var.tke_files_path
@@ -16,8 +16,8 @@ module "hpcs_init" {
 
 # Upload signed keys / tke files to provided COS bucket
 module "upload_to_cos" {
-  source = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/ibm-hpcs-initialisation/upload-to-cos?ref=hpcs-init"
-  # source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/ibm-hpcs-initialisation/upload-to-cos"
+  source = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/upload-to-cos?ref=hpcs-init"
+  # source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/upload-to-cos"
   depends_on         = [module.hpcs_init]
   api_key            = var.api_key
   cos_crn            = var.cos_crn
@@ -29,8 +29,8 @@ module "upload_to_cos" {
 
 # Remove tke files from local path
 module "remove_tke_files" {
-  # source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/ibm-hpcs-initialisation/remove-tkefiles?ref=hpcs-init"
-  source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/ibm-hpcs-initialisation/remove-tkefiles"
+  # source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/remove-tkefiles?ref=hpcs-init"
+  source             = "git::https://github.com/slzone/terraform-ibm-hpcs.git//modules/remove-tkefiles"
   depends_on         = [module.upload_to_cos]
   tke_files_path     = var.tke_files_path
   input_file_name    = var.input_file_name
