b1f57b77481edc06e15dce06b52afa7506fdefc2
Aparna Mane <aparna.mane@ibm.com>
2021-05-18 13:24:52 -0400

    Removed json file variable

--- modules/remove-tkefiles/variables.tf
+++ modules/remove-tkefiles/variables.tf
@@ -8,11 +8,7 @@ variable "tke_files_path" {
   type        = string
   description = "Path to which tke files has to be exported"
 }
-# Input Json file
-variable "input_file_name" {
-  type        = string
-  description = "Input json file name that is present in the cos-bucket or in the local"
-}
+
 variable "hpcs_instance_guid" {
   type        = string
   description = "HPCS Instance GUID"
