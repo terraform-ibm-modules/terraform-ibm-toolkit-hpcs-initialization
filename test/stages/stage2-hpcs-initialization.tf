4fcfade81b091c54581db636d90c92c9486d01c8
Sean Sundberg <seansund@us.ibm.com>
2021-06-02 10:47:28 -0500

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-hpcs-initialization.tf
+++ test/stages/stage2-hpcs-initialization.tf
@@ -1,7 +1,6 @@
 module "hpcs_initialization" {
   source = "./module"
 
-  resource_group_name = module.hpcs_resource_group.name
   initialize          = true
   tke_files_path      = "./tke_files"
   hpcs_instance_guid  = module.hpcs.guid
